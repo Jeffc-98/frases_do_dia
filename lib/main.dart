121650a462458500f8ef000a31cdb2381e603dc7
Jeffc-98 <jefferson_am1998@hotmail.com>
2026-04-14 23:04:17 -0400

    Organizei o código em classes

--- lib/main.dart
+++ lib/main.dart
@@ -3,21 +3,32 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          title: const Text("Frases do dia"),
-          backgroundColor: Colors.blue,
-        ),
-        body: Center(
-          child: TextButton(
-            onPressed: () {
-              print("Botão clicado");
-            },
-            child: const Text("pcanhagit"),
-          ),
-        ),
-      ),
+      home: Home(),
     ),
   );
+}
+
+class Home extends StatelessWidget {
+  const Home({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Text("Frases do dia"),
+        backgroundColor: Colors.blue,
+      ),
+      body: Center(
+        child: TextButton(
+          onPressed: () {
+            print("Botão clicado");
+          },
+          child: const Text("Botão"),
+        ),
+      ),
+    );
+  }
 }
