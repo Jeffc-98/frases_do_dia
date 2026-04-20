d3878a0763f792ff532cfc5d3cf8e19d26995c42
Jeffc-98 <jefferson_am1998@hotmail.com>
2026-04-20 01:28:37 -0400

    Iniciando Frases do dia

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,10 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-       
-        appBar: AppBar(
-          backgroundColor: Colors.blueAccent,
-          title: Text("Frases do dia"),
-          centerTitle: true,
-        ),
-        body:Padding(
-          padding: EdgeInsets.all(16),
-          child: Text("Conteúdo Principal"),
-        ) ,
-        bottomNavigationBar: BottomAppBar(
-          color: Colors.blueAccent,
-          child: Padding(
-            padding: EdgeInsets.all(16),
-            child: Row(
-              children: [
-                Text("Texto 1"),
-                Text("Texto 2")
-              ],
-            ),
-          ),
-        ),
+  runApp( MaterialApp(
+    home: ,
+  ));
 
-      ),
-      
 
-    )
-  );
 }
+stful
